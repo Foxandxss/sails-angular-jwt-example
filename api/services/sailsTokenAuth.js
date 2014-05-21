3157d5fb3e055848a4c341dc92fb406b7e88a784
Jesus Rodriguez <Foxandxss@gmail.com>
2014-05-21 17:20:12 +0200

    Make sailsTokenAuth more readable

--- api/services/sailsTokenAuth.js
+++ api/services/sailsTokenAuth.js
@@ -1,10 +1,19 @@
 var jwt = require('jsonwebtoken');
 
+// With this method we generate a new token based on payload we want to put on it
 module.exports.issueToken = function(payload) {
-  var token = jwt.sign(payload, process.env.TOKEN_SECRET || "please use an env variable better :P");
-  return token;
+  return jwt.sign(
+          payload, // This is the payload we want to put inside the token
+          process.env.TOKEN_SECRET || "oursecret" // Secret string which will be used to sign the token
+        );
 };
 
+// Here we verify that the token we received on a request hasn't be tampered with.
 module.exports.verifyToken = function(token, verified) {
-  return jwt.verify(token, process.env.TOKEN_SECRET || "please use an env variable better :P", {}, verified);
+  return jwt.verify(
+            token, // The token to be verified
+            process.env.TOKEN_SECRET || "oursecret", // The secret we used to sign it.
+            {}, // Options, none in this case
+            verified // The callback to be call when the verification is done.
+         );
 };
